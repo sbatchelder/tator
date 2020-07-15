049f6b1d66684ebc09af7bb3e6977099fd76d8f3
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-07-15 02:36:42 +0000

    Implement function to set ROI

--- main/static/js/annotation/fill-track.js
+++ main/static/js/annotation/fill-track.js
@@ -61,6 +61,40 @@ class FillTrack {
   }
 
   _setRoi(latest, frame) {
+    // hardcode the initial location of window
+    const x = Math.round(latest.x * this._width);
+    const y = Math.round(latest.y * this._height);
+    const w = Math.round(latest.width * this._width);
+    const h = Math.round(latest.height * this._height);
+    let trackWindow = new cv.Rect(x, y, w, h);
+
+    // set up the ROI for tracking
+    let roi = frame.roi(trackWindow);
+    let hsvRoi = new cv.Mat();
+    cv.cvtColor(roi, hsvRoi, cv.COLOR_RGBA2RGB);
+    cv.cvtColor(hsvRoi, hsvRoi, cv.COLOR_RGB2HSV);
+    let mask = new cv.Mat();
+    let lowScalar = new cv.Scalar(30, 30, 0);
+    let highScalar = new cv.Scalar(180, 180, 180);
+    let low = new cv.Mat(hsvRoi.rows, hsvRoi.cols, hsvRoi.type(), lowScalar);
+    let high = new cv.Mat(hsvRoi.rows, hsvRoi.cols, hsvRoi.type(), highScalar);
+    cv.inRange(hsvRoi, low, high, mask);
+    let roiHist = new cv.Mat();
+    let hsvRoiVec = new cv.MatVector();
+    hsvRoiVec.push_back(hsvRoi);
+    cv.calcHist(hsvRoiVec, [0], mask, roiHist, [180], [0, 180]);
+    cv.normalize(roiHist, roiHist, 0, 255, cv.NORM_MINMAX);
+
+    // delete useless mats.
+    roi.delete(); hsvRoi.delete(); mask.delete(); low.delete(); high.delete(); hsvRoiVec.delete();
+
+    // Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+    let termCrit = new cv.TermCriteria(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1);
+
+    this._hsv = new cv.Mat(this._height, this._width, cv.CV_8UC3);
+    this._dst = new cv.Mat();
+    this._hsvVec = new cv.MatVector();
+    this._hsvVec.push_back(this._hsv);
   }
 
   _predict(frame) {
